6dab33d203fa19ba31c978928cb57979433d91ef
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2021-04-04 16:24:21 +0800

    动态生成列

--- NodeJs/10协议-服务器/server/index.js
+++ NodeJs/10协议-服务器/server/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-07 19:46:38
+ * @LastEditTime: 2021-04-04 16:00:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /The-self-cultivation-of-a-programmer-NodeJs/NodeJs/10协议-服务器/server/index.js
+ */
 let LcApp = require('./lcApp');
 let app = new LcApp();
 
@@ -29,23 +37,40 @@ app.on('/movies', function (req, res) {
             name: '毛里塔尼亚人',
             brief: '影片改编自Mohamedou Ould Slahi的回忆录《关塔那摩日记》，讲述Slahi(塔哈饰)被美国政府俘虏，在毫无指控与判决的情况下被关进了监狱，他失去了希望。Slahi寻得辩护律师南希·霍兰德(福斯特饰)与助手特里·邓肯(伍德蕾饰)的帮助，他们在面对无数障碍的情况下，一同追求正义。在富有争议的主张及斯图尔特·库什中尉(康伯巴奇饰)揭露的证据下，一个令人震惊的阴谋露出水面。',
             author: '凯文·麦克唐纳 / Kevin Macdonald',
+            list: ["小明", 'TFBOY', '蔡徐坤']
         },
         {
             name: '人生可以重来',
             brief: '两个重复生活在同一天的青少年，使他们能够创造出有名无实的地图。',
             author: '伊恩·塞缪尔斯 / Ian Samuels',
+            list: [
+                {
+                    name: '成龙',
+                    gender: 'male',
+                },
+                {
+                    name: '刘德华',
+                    gender: 'male',
+                },
+                {
+                    name: '周星驰',
+                    gender: 'male',
+                }
+            ]
         },
     ]
-    let index = res.pathObj.base,
-        pageData = movies[index];
-
-    res.render(pageData, './template/movie.html')
-    // res.end(movies[index].name);
-
+    let index = res.pathObj.base;
+    if (index == 0) {
+        res.render(movies[index], './template/templateZero.html')
+    } else if (index == 1) {
+        res.render(movies[index], './template/templateOne.html')
+    } else {
+        res.end('<h1>404</h1>');
+    }
 })
 
 
 // 服务启动;
 app.run(80, () => {
-    console.info(`服务已启动: http://192.168.3.23:80`)
+    console.info(`服务已启动: http://192.168.3.3:80`)
 })
--- NodeJs/10协议-服务器/server/lcApp.js
+++ NodeJs/10协议-服务器/server/lcApp.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-07 19:34:33
- * @LastEditTime: 2021-04-03 16:44:47
+ * @LastEditTime: 2021-04-04 15:40:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /The-self-cultivation-of-a-programmer/WEBFrontEnd/NodeJS/Basics/10协议-服务器/server/lcApp.js
@@ -78,23 +78,59 @@ function render(options, templatePath) {
         if (err) {
             throw new Error(err);
         } else {
-            let reg = /\{\{(.*?)\}\}/igs,
-                result;
-            // result = reg.exec(data)操作赋值后返回的是reg.exec(data)的结果;
-            // exec函数如果没有匹配完成会一直返回匹配结果, 如果完成后没有匹配结果返回null;
-            // reg.exec('{{title}} abc sdf sdfs {{name}}')
-            // 不理解用while循环的原因 可以将以上正则的exec函数匹配结果在控制台执行多次查看结果;
-            while (result = reg.exec(data)) {
-                let strKey = result[1].trim();
-                let strVal = options[strKey];
-                data = data.replace(result[0], strVal);
-            }
+            data = replaceArr(data, options);
+            data = replaceVal(data, options);
+
             // 这里使用箭头函数, 所以当前的this指向的就是response
             this.end(data);
         }
     })
 }
 
+/**
+ * @description: 
+ * @param {*} data
+ * @param {*} options
+ * @return {*}
+ */
+function replaceVal(data, options) {
+    // 匹配普通的变量,并替换内容
+    let reg = /\{\{(.*?)\}\}/igs,
+        result;
+    // result = reg.exec(data)操作赋值后返回的是reg.exec(data)的结果;
+    // exec函数如果没有匹配完成会一直返回匹配结果, 如果完成后没有匹配结果返回null;
+    // reg.exec('{{title}} abc sdf sdfs {{name}}')
+    // 不理解用while循环的原因 可以将以上正则的exec函数匹配结果在控制台执行多次查看结果;
+    while (result = reg.exec(data)) {
+        let strKey = result[1].trim();
+        let strVal = eval('options.' + strKey);
+        data = data.replace(result[0], strVal);
+    }
+    return data;
+}
 
+
+/**
+ * @description: 
+ * @param {*} data
+ * @param {*} options
+ * @return {*}
+ */
+function replaceArr(data, options) {
+    // 匹配循环的变量, 并且替换循环的内容;
+    let reg = /\{\%for \{(.*?)\}\%\}(.*?)\{\%endfor\%\}/igs,
+        result;
+    while (result = reg.exec(data)) {
+        let strKey = result[1].trim();
+        // 通过Key值获取数组内容;
+        let strVal = options[strKey],
+            listStr = "";
+        strVal.forEach((item, ind) => {
+            listStr = listStr + replaceVal(result[2], { "item": item });
+        })
+        data = data.replace(result[0], listStr);
+    }
+    return data;
+}
 
 module.exports = LcApp;
